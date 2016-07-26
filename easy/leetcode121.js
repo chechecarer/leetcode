7494a8fcbe9926934c11a97a9428b22e7deded12
carrie <carrie_0612@sina.com>
2016-07-26 08:39:37 +0800

    update 121

--- easy/leetcode121.js
+++ easy/leetcode121.js
@@ -1,20 +1,36 @@
-var maxProfit = function(prices){
-	if(prices.length <= 1){
-		return 0;
-	}
-	var min = prices[0];
-	var max = prices[1];
-	var i;
-	for(i=1; i<prices.length; i++){
-		if(prices[i] < min){
-			min = prices[i];
-			if(prices[i+1] == 'undefined'){
-				return 0;
-			}else{
-				max = prices[i+1];
-			}
-		}else{
-			
-		}
-	}
-}
+var maxProfit = function(prices) {
+    if(prices.length <= 1){
+        return 0;
+    }
+    var min = 0;
+    var max = 1;
+    var result = prices[max] - prices[min];
+    var i;
+    for(i=1; i<prices.length; i++){
+        if(prices[i] < prices[min] ){
+            if(i>max){
+                if(result < (prices[max]-prices[min])){
+                    result = prices[max]-prices[min];
+                }
+            }
+            min = i;
+            if(i+1 != prices.length){
+                max = i+1;
+            }else{
+                break;
+            }
+        }else{
+            if(prices[i] > prices[max] ){
+                max = i;
+            }
+        }
+    }
+    if(max > min){
+        result = (prices[max]-prices[min])>result? (prices[max]-prices[min]):result;
+    }
+    console.log('max='+max+',min='+min);
+    if(result < 0){
+        result = 0;
+    }
+    return result;
+};
