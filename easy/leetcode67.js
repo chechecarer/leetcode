cb2c077c33b842f10417107beb295bdc103f700b
carrie <carrie_0612@sina.com>
2016-07-28 22:31:45 +0800

    add some problems' solutions

--- easy/leetcode67.js
+++ easy/leetcode67.js
@@ -1,55 +1,65 @@
-var addBinary = function(a, b){
-	// var na = parseInt(a, 2);
-	// var nb = parseInt(b, 2);
-	// var result = na+nb;
-	// return result.toString(2);
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
 
-	var obj = {
-		'0': 0,
-		'1': 1
-	};
-	var al = a.length;
-	var bl = b.length;
-	var result = '';
-	var c = 0;
-	var i,j;
-    console.log('al='+al+',bl='+bl);
-	if(al > bl){
-		j = al-1;
-		c = 0;
-		for(i=bl-1 ; i>=0; i--){
-		    console.log(a[j]);
-		    console.log(b[i]);
-		    console.log(obj[a[j]]+obj[b[i]]+c);
-			if(obj[a[j]]+obj[b[i]]+c == 2){
-				c = 1;
-				a.splice(a.length-1,1);
-				a += '0';
-			}else{
-				a[j] = obj[a[j]]+obj[b[i]]+c +'';
-				c = 0;
-			}
-			j --;
-		}
-		if(c == 1){
-			result = '1'+a;
-		}
-	}else{
-		j = bl.length-1;
-		c = 0;
-		for(i=al-1 ; i>=0; i--){
-			if(obj[b[j]]+obj[a[i]]+c == 2){
-				c = 1;
-				b[j] = '0';
-			}else{
-				b[j] = obj[b[j]]+obj[a[i]]+c+'';
-				c = 0;
-			}
-			j --;
-		}
-		if(c == 1){
-			result = '1'+b;
-		}
-	}
+var addBinaryFirst = function(a, b){
+	var an = parseInt(a, 2);
+	var bn = parseInt(b, 2);
+	var re = an +bn;
+	var result = re.toString(2);
 	return result;
 }
+
+var addBinary = function(a, b) {
+   var obj = {
+        '0': 0,
+        '1': 1
+    };
+    var result = '';
+    var c = 0;
+    var j = a.length-1;
+    var i = b.length-1;
+    while(i>=0 &&j>=0){
+        if(obj[a[j]]+obj[b[i]]+c >= 2){
+            result = obj[a[j]]+obj[b[i]]+c-2+''+result;
+            c = 1;
+        }else{
+            result = obj[a[j]]+obj[b[i]]+c+''+result;
+            c = 0;
+        }
+        j --;
+        i --;
+    }
+    if(i>=0){
+        while(i>=0){
+            if(obj[b[i]]+c >= 2){
+                result = obj[b[i]]+c-2+''+result;
+                c = 1;
+
+            }else{
+                result = obj[b[i]]+c +''+ result;
+                c = 0;
+            }
+            i --;
+        }
+    }
+    if(j>=0){
+        while(j>=0){
+            if(obj[a[j]]+c >= 2){
+                result = obj[a[j]]+c-2+''+result;
+                c = 1;
+
+            }else{
+                result = obj[a[j]]+c+''+result;
+                c = 0;
+            }
+            j --;
+        }
+    }
+    if(c == 1){
+        result = '1'+ result;
+    }
+    return result;
+};
